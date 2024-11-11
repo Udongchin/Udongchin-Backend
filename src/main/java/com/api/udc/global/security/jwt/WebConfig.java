8d9089e051e39b2764141069e91c3da54559085d
ijunsoo <dlwnstni74@gmail.com>
2024-11-11 15:25:26 +0900

    test

--- src/main/java/com/api/udc/global/security/jwt/WebConfig.java
+++ src/main/java/com/api/udc/global/security/jwt/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000","http://localhost:5000") // 허용할 출처. 모든 도메인 허용하려면 "*"
+                .allowedOrigins("http://localhost:3000","http://localhost:5000","http://localhost:5173") // 허용할 출처. 모든 도메인 허용하려면 "*"
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true); // 인증 정보를 포함할 수 있도록 허용
